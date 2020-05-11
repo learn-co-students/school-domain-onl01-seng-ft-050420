e81beb91ab6401644644b755426e5a704f8a9acd
claudia v <claudia4ro@yahoo.com>
2020-05-11 22:39:30 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,43 @@
-# code here!
+
+class School
+  
+  attr_reader :name, :roster, :grade
+  
+  
+  
+  def initialize (name)
+    @name = name
+    @roster = Hash.new { |hash, key| hash[key] = [] }
+    
+    
+  end
+  
+  def add_student (name, grade)
+  
+    #roster[grade]=[]
+    if @roster.has_key?(grade)
+      @roster[grade] <<  name 
+    else
+      #@roster[grade] 
+      
+      @roster [grade] << name
+    end
+    
+    
+  end
+  
+  def grade (grade)
+    @roster.values_at(grade).flatten
+  end
+  
+  def sort
+    new_roster = {}
+    @roster.collect do | grade, names |
+      new_roster[grade] = names.sort
+    end
+    new_roster
+      
+  end
+  
+  
+end
