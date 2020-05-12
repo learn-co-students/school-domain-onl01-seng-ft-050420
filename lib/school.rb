7f132f361a764f648b5c431c8154670c5866c519
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-05-12 01:25:56 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+require 'pry'
+class School
+  def initialize(school)
+    @school = school 
+    @roster = Hash.new
+  end 
+  
+  def roster
+     @roster
+  end 
+    
+  def add_student(student, grade)
+    if @roster.keys.include?(grade)
+      @roster[grade] << student
+    else 
+      @roster[grade] = []
+      @roster[grade].push(student)
+    end 
+  end 
+
+  def grade(grade)
+    @roster[grade]
+  end 
+
+  def sort
+    @roster.each do |grade, student|
+      @roster[grade] = student.sort
+    end
+  end
+binding.pry
+end 
